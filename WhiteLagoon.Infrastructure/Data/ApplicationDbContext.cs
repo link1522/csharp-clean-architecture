320211814e2eaecf56e462e55bf885306dbc0b64
Link1515 <h26281660@gmail.com>
2024-09-19 16:46:40 +0800

    add some data to villas table

--- WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
+++ WhiteLagoon.Infrastructure/Data/ApplicationDbContext.cs
@@ -15,5 +15,40 @@ namespace WhiteLagoon.Infrastructure.Data
         }
 
         public DbSet<Villa> Villas { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Villa>().HasData(
+            new Villa {
+                Id = 1,
+                Name = "Royal Villa",
+                Description = "lorem ipsum",
+                ImageUrl = "https://placehold.co/600x400",
+                Occupancy = 4,
+                Price = 200,
+                Sqft = 550,
+            },
+            new Villa {
+                Id = 2,
+                Name = "Pool Villa",
+                Description = "lorem ipsum",
+                ImageUrl = "https://placehold.co/600x400",
+                Occupancy = 4,
+                Price = 300,
+                Sqft = 550,
+            },
+            new Villa {
+                Id = 3,
+                Name = "Luxury Villa",
+                Description = "lorem ipsum",
+                ImageUrl = "https://placehold.co/600x400",
+                Occupancy = 4,
+                Price = 400,
+                Sqft = 700,
+            });
+        }
+
     }
 }
