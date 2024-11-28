ac98e8fade903160cdb5f757627b7d660e8a70f1
Link1515 <h26281660@gmail.com>
2024-11-28 17:34:40 +0800

    show villa number list

---
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public VillaNumberController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var villaNumbers = await _context.VillaNumbers.ToListAsync();
+            return View(villaNumbers);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Villa obj)
+        {
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("Description", "The description cannot exactly match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _context.Villas.AddAsync(obj);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "Villa has been created successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Update(int id)
+        {
+            Villa? obj = await _context.Villas.FindAsync(id);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Villa obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Villas.Update(obj);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "Villa has been updated successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Villa? obj = await _context.Villas.FindAsync(id);
+
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Villa obj)
+        {
+            var objFromDb = await _context.Villas.FindAsync(obj.Id);
+
+            if (objFromDb is not null)
+            {
+                _context.Villas.Remove(objFromDb);
+                await _context.SaveChangesAsync();
+                TempData["success"] = "Villa has been deleted successfully";
+                return RedirectToAction("Index");
+            }
+
+            TempData["error"] = "The Villa could not be deleted";
+            return View(obj);
+        }
+    }
+}
