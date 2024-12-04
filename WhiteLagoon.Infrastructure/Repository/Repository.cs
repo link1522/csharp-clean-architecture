0a244ecb47cf194fb273fa98edb1ab2cffd50877
Link1515 <h26281660@gmail.com>
2024-12-04 17:15:02 +0800

    create Repository Base class

---
+++ WhiteLagoon.Infrastructure/Repository/Repository.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+using WhiteLagoon.Application.Common.Interfaces;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Infrastructure.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext context)
+        {
+            _context = context;
+            dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter, string? includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter is not null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries)) 
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>>? filter = null, string? includeProperties = null)
+        {
+            IQueryable<T> query = dbSet;
+
+            if (filter is not null)
+            {
+                query = query.Where(filter);
+            }
+
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProperty in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries)) 
+                {
+                    query = query.Include(includeProperty);
+                }
+            }
+
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+    }
+}
--- WhiteLagoon.Infrastructure/Repository/VillaRepository.cs
+++ WhiteLagoon.Infrastructure/Repository/VillaRepository.cs
@@ -1,68 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Linq.Expressions;
-using WhiteLagoon.Application.Common.Interfaces;
+﻿using WhiteLagoon.Application.Common.Interfaces;
 using WhiteLagoon.Domain.Entities;
 using WhiteLagoon.Infrastructure.Data;
 
 namespace WhiteLagoon.Infrastructure.Repository
 {
-    public class VillaRepository : IVillaRepository
+    public class VillaRepository : Repository<Villa>, IVillaRepository
     {
         private readonly ApplicationDbContext _context;
 
-        public VillaRepository(ApplicationDbContext context)
+        public VillaRepository(ApplicationDbContext context): base(context)
         {
             _context = context;
-        }
-
-        public void Add(Villa entity)
-        {
-            _context.Add(entity);
-        }
-
-        public Villa Get(Expression<Func<Villa, bool>> filter, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _context.Set<Villa>();
-
-            if (filter is not null)
-            {
-                query = query.Where(filter);
-            }
-
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                foreach (var includeProperty in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries)) 
-                {
-                    query = query.Include(includeProperty);
-                }
-            }
-
-            return query.FirstOrDefault();
-        }
-
-        public IEnumerable<Villa> GetAll(Expression<Func<Villa, bool>>? filter = null, string? includeProperties = null)
-        {
-            IQueryable<Villa> query = _context.Set<Villa>();
-
-            if (filter is not null)
-            {
-                query = query.Where(filter);
-            }
-
-            if (!string.IsNullOrEmpty(includeProperties))
-            {
-                foreach (var includeProperty in includeProperties.Split(new char[] {','}, StringSplitOptions.RemoveEmptyEntries)) 
-                {
-                    query = query.Include(includeProperty);
-                }
-            }
-
-            return query.ToList();
-        }
-
-        public void Remove(Villa entity)
-        {
-            _context.Remove(entity);
         }
 
         public void Save()
