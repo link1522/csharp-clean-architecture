6f321bafad662f07254180549734f62449ab5153
Link1515 <h26281660@gmail.com>
2024-12-10 16:18:25 +0800

    add Users Table to DB

---
+++ WhiteLagoon.Domain/Entities/User.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WhiteLagoon.Domain.Entities
+{
+    public class User
+    {
+        [Key]
+        public required int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string PasswordHash { get; set; }
+
+        public bool VerifyPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+            var hashedPassword = Convert.ToBase64String(sha256.ComputeHash(Encoding.UTF8.GetBytes(password)));
+            return hashedPassword == PasswordHash;
+        }
+    }
+}
